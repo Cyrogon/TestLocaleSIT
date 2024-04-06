9d7d165d199f6daf5326188f06cedca501c387c7
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-07 00:22:59 +0100

    trying to add de more

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,11 +61,14 @@ if build_all_docs is not None:
   if (current_version == 'latest'):
     html_context['languages'].append(['en', pages_root+'/en'])
     html_context['languages'].append(['zh_cn', pages_root+'/zh_cn'])
+    html_context['languages'].append(['de', pages_root+'/de'])
 
   if (current_language == 'en'):
     html_context['versions'].append(['latest', pages_root+'/en'])
   if (current_language == 'zh_cn'):
     html_context['versions'].append(['latest', pages_root+'/zh_cn'])
+  if (current_language == 'de'):
+    html_context['versions'].append(['latest', pages_root+'/de'])
 
   # and loop over all other versions from our yaml file
   # to set versions and languages
