68d32faa2f2408718fa0137c17a7baad0a7df082
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-06 17:42:51 +0100

    Remove /'s

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,13 +60,13 @@ if build_all_docs is not None:
   # and we append all versions and langauges accordingly 
   # we treat the main branch as latest 
   if (current_version == 'latest'):
-    html_context['languages'].append(['en', pages_root+'/en'])
-    html_context['languages'].append(['zh_cn', pages_root+'/zh_cn'])
+    html_context['languages'].append(['en', pages_root+'en'])
+    html_context['languages'].append(['zh_cn', pages_root+'zh_cn'])
 
   if (current_language == 'en'):
-    html_context['versions'].append(['latest', pages_root+'/en'])
+    html_context['versions'].append(['latest', pages_root+'en'])
   if (current_language == 'zh_cn'):
-    html_context['versions'].append(['latest', pages_root+'/zh_cn'])
+    html_context['versions'].append(['latest', pages_root+'zh_cn'])
 
   # and loop over all other versions from our yaml file
   # to set versions and languages
