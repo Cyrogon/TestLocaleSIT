9fe77042d4e951c4e32bc66cb8a4d8600e03e0e7
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-06 17:05:39 +0100

    maybe?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['source/_static']
+html_static_path = ['/_static']
 html_theme = 'sphinx_rtd_theme'
-locale_dirs = ['/locales']
+locale_dirs = ['../locales']
 
 html_context = {
   'current_version' : "latest",
