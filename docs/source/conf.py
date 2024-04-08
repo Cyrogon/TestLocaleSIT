32fbcb3f454fb0dfe1bd36eb614d6face64e2a9c
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-09 00:23:28 +0100

    Made tweaks that should change nothing in theory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,60 +33,37 @@ html_scaled_image_link = False
 langs = ["en"]
 langs += os.listdir("../locales")
 
-html_context = {
-  'current_version' : "latest",
-  'versions' : [],
-  'current_language': 'en',
-  'languages': langs
-}
-
 # get the environment variable build_all_docs and pages_root
 build_all_docs = os.environ.get("build_all_docs", str(True))
 pages_root = os.environ.get("pages_root", "https://cyrogon.github.io/TestLocaleSIT")
 
-# if not there, we dont call this
-if build_all_docs is not None:
-  # we get the current language and version
-  current_language = os.environ.get("current_language")
-  current_version = os.environ.get("current_version")
+current_language = os.environ.get("current_language")
+current_version = os.environ.get("current_version")
 
-  # we set the html_context wit current language and version 
-  # and empty languages and versions for now
-  html_context = {
-    'current_language' : current_language,
-    'languages' : [],
-    'current_version' : current_version,
-    'versions' : [],
-  }
+html_context = {
+  'current_language' : current_language,
+  'languages' : [],
+  'current_version' : current_version,
+  'versions' : [],
+}
 
-  # and we append all versions and langauges accordingly 
-  # we treat the main branch as latest 
-  if (current_version == 'latest'):
-    html_context['languages'].append(['en', pages_root+'/en'])
-    html_context['languages'].append(['zh_CN', pages_root+'/zh_CN'])
-    html_context['languages'].append(['de', pages_root+'/de'])
-    html_context['languages'].append(['fr', pages_root+'/fr'])
-    html_context['languages'].append(['es', pages_root+'/es'])
+if (current_version == 'latest'):
+  for lang in langs:
+    html_context['languages'].append([lang, "{}/{}".format(pages_root, lang)])
 
-  if (current_language == 'en'):
-    html_context['versions'].append(['latest', pages_root+'/en'])
-  if (current_language == 'zh_CN'):
-    html_context['versions'].append(['latest', pages_root+'/zh_CN'])
-  if (current_language == 'de'):
-    html_context['versions'].append(['latest', pages_root+'/de'])
-  if (current_language == 'fr'):
-    html_context['versions'].append(['latest', pages_root+'/fr'])
-  if (current_language == 'es'):
-    html_context['versions'].append(['latest', pages_root+'/es'])
+html_context['versions'].append(['latest', "{}/{}".format(pages_root, current_language)])
 
-  # and loop over all other versions from our yaml file
-  # to set versions and languages
-  with open("../versions.yaml", "r") as yaml_file:
-    docs = yaml.safe_load(yaml_file)
 
-  if (current_version != 'latest'):
-    for language in docs[current_version].get('languages', []):
-      html_context['languages'].append([language, pages_root+'/'+current_version+'/'+language])
+# and loop over all other versions from our yaml file
+# to set versions and languages
+with open("../versions.yaml", "r") as yaml_file:
+  docs = yaml.safe_load(yaml_file)
 
-  for version, details in docs.items():
-    html_context['versions'].append([version, pages_root+'/'+version+'/'+current_language])
+if (current_version != 'latest'):
+  for language in docs[current_version].get('languages', []):
+    html_context['languages'].append([language, "{}/{}/{}".format(pages_root, current_version, language)])
+
+
+
+for version, details in docs.items():
+  html_context['versions'].append([version, "{}/{}/{}".format(pages_root, version, current_language)])
