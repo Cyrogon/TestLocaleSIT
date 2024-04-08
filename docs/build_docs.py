89bc5b0bb683f4d48c1bd1fa850e5c84d0015701
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-08 02:06:07 +0100

    commented out building

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -35,6 +35,7 @@ print(os.getcwd())
 langs = os.listdir("locales")
 
 # Iterate over all found languages and build then move each one to it's designated dir
+"""
 for i in langs:
    build_doc("latest", i, "py-rewrite")
    move_dir("./build/html/", "../pages/{}".format(i))
@@ -49,3 +50,4 @@ for version, details in docs.items():
   for language in details.get('languages', []): 
     build_doc(version, language, version)
     move_dir("./build/html/", "../pages/"+version+'/'+language+'/')
+"""
