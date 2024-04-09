ce898ab2b5199b5f41360190c6ab2ce95f4711c8
ChrissyBoyo <45185673+Cyrogon@users.noreply.github.com>
2024-04-10 00:28:11 +0100

    moved SIT docs over to new system

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ['static']
+html_static_path = ['_static']
 html_theme = 'sphinx_rtd_theme'
 locale_dirs = ['../locales']
 html_scaled_image_link = False
